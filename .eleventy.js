793d008934bf878877bf17f23900b059c0ab84b4
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-06-12 19:42:03 -0400

    Implement project cards on portfolio page

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('src/css');
   eleventyConfig.addPassthroughCopy('src/js');
+  eleventyConfig.addPassthroughCopy('src/img');
 
   return {
     dir: {
