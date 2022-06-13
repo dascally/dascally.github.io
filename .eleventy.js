f25aca3ae8e9b4e764f83aa231b7b8cf9a49ddab
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-06-13 14:36:26 -0400

    Inline collapse toggle JS

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,19 @@
+const { minify } = require('terser');
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('src/css');
   eleventyConfig.addPassthroughCopy('src/js');
   eleventyConfig.addPassthroughCopy('src/img');
+
+  eleventyConfig.addNunjucksAsyncFilter('jsmin', async (code, cb) => {
+    try {
+      const result = await minify(code);
+      cb(null, result.code);
+    } catch (err) {
+      console.error('Terser error:', err);
+      cb(null, code);
+    }
+  });
 
   return {
     dir: {
