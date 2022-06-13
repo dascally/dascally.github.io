88860e5b53c882ce5e946e307f4bd64a3c0e199f
dascally <44758401+dascally@users.noreply.github.com>
2022-06-13 18:36:55 +0000

    Deploying to gh-pages from @ dascally/dascally.github.io@f25aca3ae8e9b4e764f83aa231b7b8cf9a49ddab 🚀

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const toggleCollapseElts = document.querySelectorAll('.js-toggle-collapse');
-toggleCollapseElts.forEach((elt) => {
-  elt.addEventListener('click', (evt) => {
-    const targetElt = document.querySelector(elt.dataset.target);
-    targetElt.classList.toggle('show');
-  });
-});
