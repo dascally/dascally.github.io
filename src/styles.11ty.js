9a60d5cabf3bbb05846594a0149252742816d258
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-06-14 18:20:42 -0400

    Minify CSS

---
+++ src/styles.11ty.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const postcss = require('postcss');
+
+module.exports = class {
+  data() {
+    const cssPath = path.join(__dirname, './css/main.css');
+    const rawCSS = fs.readFileSync(cssPath);
+
+    return {
+      permalink: 'styles.css',
+      cssPath,
+      rawCSS,
+    };
+  }
+
+  async render({ cssPath, rawCSS }) {
+    return await postcss([require('postcss-import'), require('cssnano')])
+      .process(rawCSS, { from: cssPath, to: cssPath })
+      .then((result) => result.css);
+  }
+};
