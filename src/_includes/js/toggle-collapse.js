a59eed9203d1053be419ce03bc445e7c55185a09
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-07-07 22:58:51 -0400

    Improve collapse functionality
    - Add ARIA attributes
    - Improve animation by using wrapper div and calculating max-height
      with script
    - Change class names to BEMish style
    - Update global nav to use new classes and wrapper div

--- src/_includes/js/toggle-collapse.js
+++ src/_includes/js/toggle-collapse.js
@@ -1,7 +1,31 @@
-const toggleCollapseElts = document.querySelectorAll('.js-toggle-collapse');
-toggleCollapseElts.forEach((elt) => {
-  elt.addEventListener('click', (evt) => {
-    const targetElt = document.querySelector(elt.dataset.target);
-    targetElt.classList.toggle('show');
+// Wait for load event so that height calculations reflect styles
+window.addEventListener('load', (evt) => {
+  const collapseElts = document.querySelectorAll('.collapseContainer');
+
+  collapseElts.forEach((collapseElt) => {
+    let contentsHeight = Array.prototype.reduce.call(
+      collapseElt.children,
+      (sum, elt) => sum + elt.offsetHeight,
+      0
+    );
+    contentsHeight += getComputedStyle(collapseElt.lastElementChild)[
+      'margin-block-end'
+    ];
+
+    collapseElt.style['max-height'] = `${contentsHeight}px`;
+  });
+
+  const toggleElts = document.querySelectorAll('.js-collapse-toggle');
+
+  toggleElts.forEach((toggleElt) => {
+    toggleElt.addEventListener('click', (evt) => {
+      const targetID = toggleElt.getAttribute('aria-controls');
+      const collapseContainerElt = document.querySelector(`#${targetID}`);
+
+      const collapsed = collapseContainerElt.classList.toggle(
+        'collapseContainer--collapsed'
+      );
+      toggleElt.setAttribute('aria-expanded', `${!collapsed}`);
+    });
   });
 });
